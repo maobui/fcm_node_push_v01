53c6a91c0ed63d42e4b8ade2b2de1353c6bf9865
mao.buidinh <mao.buidinh@hotmail.com>
2018-07-21 02:09:42 +0700

    - use server key dynamic (input by user).

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,6 @@ const { matchedData } = require('express-validator/filter')
 
 // for FCM
 const FCM = require('fcm-node');
-const serverKey = require('./your_server_key.json'); //put your server key here
-const fcm = new FCM(serverKey);
 const topics = {key_asser:"Asser",
 				key_cezanne:"Cezanne",
 				key_jlin:"Jessica",
@@ -30,6 +28,10 @@ router.get('/contact', (req, res) => {
 
 
 router.post('/contact', [
+  check('server_key')
+    .isLength({ min: 1 })
+    .withMessage('Server key is required')
+    .trim(),
   check('message')
     .isLength({ min: 1 })
     .withMessage('Message is required')
@@ -55,6 +57,8 @@ router.post('/contact', [
   console.log('req.body.topic:', req.body.topic)
   
   // for FCM
+  const serverKey = data.server_key; //put your server key here
+  const fcm = new FCM(serverKey);
   if (data.topic === 'key_random') {
 	  var topic_keys = Object.keys(topics);
 	  var random_key = topic_keys[Math.floor(Math.random()*topic_keys.length)];
@@ -73,21 +77,20 @@ router.post('/contact', [
   fcm.send(msg, function(err, response){
 		if (err) {
 			console.log("Something has gone wrong!", err);
+			req.flash('error', 'Something has gone wrong!. Please check your server key.');
 		} else {
 			console.log("Successfully sent with response: ", response);
+			req.flash('success', 'Thanks for the message! I‘ll be in touch :).Please see the push notification message you sent.');
+			req.flash('msg', JSON.stringify(msg));
 		}
+		res.redirect('/')
 	});
-  
-  req.flash('success', 'Thanks for the message! I‘ll be in touch :).Please see the push notification message you sent.');
-  req.flash('msg', msg);
-
   
   // console.log("Smessage !  ", message);
   // for (property in message) {
   // console.log(property, '= ', JSON.stringify(message[property]));
   // }
-
-  res.redirect('/')
+  
 })
 
 module.exports = router
